3bc636fc40418cd069516d375abc390c95932b7e
Prima06112002 <ramadhan06112002@gmail.com>
2022-12-02 16:08:02 +0700

    add public void pop and public void display

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -44,6 +44,28 @@ namespace StackUsingLinkedList_CSharp
             Console.WriteLine("\n" + element + "pushed." );
         }
 
+        public void pop()
+        {
+            Console.WriteLine("\nThe popped element is: " + top.info);
+            top = top.next; //Make top point to the next node in sequence
+        }
+
+        public void display()
+        {
+            Node tmp;
+            if (empty())    //if stack is empty
+                Console.WriteLine("\nStack Empty");
+            else
+            {
+                //Traverse the list from begginning till end
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+                }
+                Console.WriteLine();
+            }
+        }
+
 
 
        
