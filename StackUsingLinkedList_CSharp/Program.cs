26d2418b3d68eb0da541ce0336c0c8c741c5ef7f
Prima06112002 <ramadhan06112002@gmail.com>
2022-12-02 16:20:03 +0700

    add stack list menu

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -65,6 +65,24 @@ namespace StackUsingLinkedList_CSharp
                 Console.WriteLine();
             }
         }
+        static void Main(string[] args)
+        {
+            Stacks s = new Stacks();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+                Console.WriteLine("1. Push.");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.Write("\nEnter your choice: ");
+                string sInput = Console.ReadLine();
+                char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
+                switch (ch)
+                { }
+            }
+        }
 
 
 
