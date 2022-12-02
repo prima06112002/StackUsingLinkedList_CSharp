25dd41c20072cbed59510d16b029d581dacc4015
Prima06112002 <ramadhan06112002@gmail.com>
2022-12-02 15:56:07 +0700

    add class Stacks and bool empty

--- StackUsingLinkedList_CSharp/Program.cs
+++ StackUsingLinkedList_CSharp/Program.cs
@@ -16,4 +16,24 @@ namespace StackUsingLinkedList_CSharp
             next = n;
         }
     }
+
+    class Stacks
+    {
+        Node top;
+
+        public Stacks()
+        {
+            top = null;
+        }
+
+        bool empty()
+        {
+            if (top == null)
+                return (true);
+            else
+                return (false);
+        }
+
+       
+    }
 }
